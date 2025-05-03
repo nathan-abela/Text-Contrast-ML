c4ab6ffa72e894660f4eea63a8e71c72af20c8cd
Nathan Abela <nathanabela7@gmail.com>
2025-05-03 20:01:05 +0200

    chore(next config): add comments for clarity

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 
+// Determines if the current environment is production
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+	/**
+	 * Sets the base path for the application.
+	 * For deploying to a subdirectory (ex. GitHub Pages).
+	 * Uses '/Text-Contrast-ML' in production, empty otherwise.
+	 */
 	basePath: isProd ? "/Text-Contrast-ML" : "",
+	/**
+	 * Sets the asset prefix for the application.
+	 * For serving assets from a different domain.
+	 * Uses '/Text-Contrast-ML' in production, empty otherwise.
+	 */
 	assetPrefix: isProd ? "/Text-Contrast-ML" : "",
 	output: "export",
 };
